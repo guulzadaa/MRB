50a9b79783f8c84d6cdca12a04a022df6dab22fe
imaknvg <kamiten03@mail.ru>
2026-02-07 20:01:23 +0500

    Update RewardToken.sol
    Code

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -1,2 +1,27 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RewardToken is ERC20, Ownable {
+    address public minter;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address owner_
+    )
+        ERC20(name_, symbol_)
+        Ownable(owner_)  
+    {}
+
+    function setMinter(address _minter) external onlyOwner {
+        minter = _minter;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == minter, "not minter");
+        _mint(to, amount);
+    }
+}
