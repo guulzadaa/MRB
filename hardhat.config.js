797f8f2c9111e53841b6f13bb99add0a3034bf20
aiainel <anel.kashym@mail.ru>
2026-02-07 17:22:53 +0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,3 +9,15 @@ module.exports = {
     },
   },
 };
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
